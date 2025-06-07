f169efa0b99786a5719bd24c52f7a563f32cc68a
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 11:23:40 +0530

    Added one Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SpringbootConnection';
+
+  constructor(private http: HttpClient) { }
+
+  getData(): void {
+    const url = 'http://localhost:3080/Students';
+
+    this.http.get<any>(url)
+      .pipe(
+        catchError(error => {
+          console.error('Error occurred', error);
+          return of({});
+        })
+      )
+      .subscribe(response => {
+        console.log('Response:', response);
+      });
+  }
+
 }
